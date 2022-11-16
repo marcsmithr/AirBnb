7b43ea5883fae7786e523dc22ebd324e3fb7a813
marc <marcsmithr@gmail.com>
2022-11-16 15:32:42 -0600

    phase 0 completed

---
+++ backend/routes/index.js
@@ -0,0 +1,11 @@
+// backend/routes/index.js
+// ...
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+  });
+  // ...
