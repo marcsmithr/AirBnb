ccd4e2adcf40005bc4335a213d2c614787689f5b
marc <marcsmithr@gmail.com>
2022-11-16 15:58:39 -0600

    phase 2.1 completed

--- backend/app.js
+++ backend/app.js
@@ -19,14 +19,12 @@ if (!isProduction) {
     // enable cors only in development
     app.use(cors());
   }
-
   // helmet helps set a variety of headers to better secure your app
   app.use(
     helmet.crossOriginResourcePolicy({
       policy: "cross-origin"
     })
   );
-
   // Set the _csrf token and create req.csrfToken method
   app.use(
     csurf({
@@ -38,10 +36,45 @@ if (!isProduction) {
     })
   );
 
-  // backend/app.js
-
-// ...
-
 app.use(routes); // Connect all the routes
+
+
+//ERROR HANDLERS***
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+const { ValidationError } = require('sequelize');
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
+
+
+
+
+
 
   module.exports = app;
