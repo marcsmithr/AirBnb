8fa22f0f490881d5229b756ea1df1d83245d18c6
marc <marcsmithr@gmail.com>
2022-11-23 11:27:03 -0600

    getAllSpots passed testing locally

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,6 +4,7 @@ const { Spot, Review, SpotImage, sequelize } = require('../../db/models');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+//grab all spots while having access to Reviews, without showing Reveiw data
 let spots = await Spot.findAll(
     {
     include: {
@@ -13,30 +14,41 @@ let spots = await Spot.findAll(
 
 })
 
-let spotsWithRating = []
+//iterate through spots to manipulate each spot
 for(let spot of spots){
+    //grab all reviews
     let reviews = await Review.findAll({
         where: {
             spotId: spot.id
         }
             })
+        //grab all SpotImages
     let previewImage = await SpotImage.findAll({
         where: {
             spotId: spot.id,
             preview: true
             }
         })
-        previewImage = JSON.stringify(previewImage)
-    let url = previewImage[0].url //this is undefined for some reason, previewImages returns an object with the url inside
-    console.log(previewImage)
+        //Turn previewImage into something that can be keyed into
+        let previewImageString = JSON.stringify(previewImage)
+        let previewImageObj = JSON.parse(previewImageString)
+        //grab url from previewImage
+        let url;
+    if(previewImageObj[0]){
+        url = previewImageObj[0].url
+    }
 
+    //itterate through review to add all the star reviews
     let reviewSum = 0;
     for (let review of reviews){
          reviewSum += review.stars
      }
     let reviewAverage = reviewSum / reviews.length;
+    //set the data into each spot
     spot.setDataValue('avgRating', reviewAverage)
+    if(url){
     spot.setDataValue('previewImage', url)
+    }
 
 }
 
