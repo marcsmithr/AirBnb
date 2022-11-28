30fadc27dcd2d80a5d23647f0f3fdaa7e7e176f6
marc <marcsmithr@gmail.com>
2022-11-28 15:02:45 -0600

    completed edit booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -27,6 +27,50 @@ res.status(200).send({
 })
 })
 
+router.put('/:bookingId', restoreUser, requireAuth, async(req, res)=>{
+    const{startDate, endDate} = req.body
+    console.log(startDate)
+    console.log(endDate)
+    let userDataObj = req.user
+    let userObjString = JSON.stringify(userDataObj)
+    let user = JSON.parse(userObjString)
+
+    let bookingDataObj = await Booking.findByPk(req.params.bookingId)
+    if(!bookingDataObj){
+        return res.status(404).send({
+            message: "Booking could not be found",
+            statusCode: 404
+        })
+    }
+    let bookingObjString = JSON.stringify(bookingDataObj)
+    let bookingObj = JSON.parse(bookingObjString)
+
+    if(user.id !== bookingObj.userId){
+        return res.status(403).send({
+            message: "Only the booking owner can edit their rental",
+            statusCode: 403
+        })
+    }
+
+    if(endDate < startDate){
+        return res.status(400).send({
+            message: "Validation error",
+            statusCode: 400,
+            errors: {
+                endDate: "endDate cannot come before startDate"
+            }
+        })
+    }
+
+
+    bookingDataObj.set({
+        startDate,
+        endDate
+    })
+    console.log(bookingDataObj)
+    return res.json(bookingDataObj)
+})
+
 
 
 
