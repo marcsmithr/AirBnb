116e5f2944f413b12f905fdbe544a639d446a090
marc <marcsmithr@gmail.com>
2022-11-28 11:52:23 -0600

    I lied I have now actually completed get bookings for current user

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -9,14 +9,18 @@ router.get('/current', restoreUser, requireAuth, async(req, res)=>{
     let userDataObj = req.user
     let userObjString = JSON.stringify(userDataObj)
     let user = JSON.parse(userObjString)
-    let userNoUsername = {}
-        userNoUsername.id = user.id;
-        userNoUsername.firstName = user.firstName;
-        userNoUsername.lastName = user.lastName;
+    // let userNoUsername = {}
+    //     userNoUsername.id = user.id;
+    //     userNoUsername.firstName = user.firstName;
+    //     userNoUsername.lastName = user.lastName;
 
     let Bookings= await Booking.findAll({where:{userId: user.id}})
     for(let bookingDataObj of Bookings){
-        bookingDataObj.setDataValue('User', userNoUsername)
+        let bookingDataString = JSON.stringify(bookingDataObj)
+        let booking = JSON.parse(bookingDataString)
+        let spot = await Spot.findAll({where:{id: booking.spotId}})
+
+        bookingDataObj.setDataValue('Spot', spot)
 }
 res.status(200).send({
     Bookings
