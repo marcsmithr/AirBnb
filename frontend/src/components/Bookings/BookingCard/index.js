2f8a6cd10dc84e00fc45f3f60747cf9fe32ad09a
marc <marcsmithr@gmail.com>
2023-04-03 20:04:10 -0500

    began booking card css

--- frontend/src/components/Bookings/BookingCard/index.js
+++ frontend/src/components/Bookings/BookingCard/index.js
@@ -1,22 +1,61 @@
 import React, { useEffect, useMemo } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getOneSpot } from "../../../store/spotReducer"
+import './index.css'
+
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+    dateStyle: "short"
+})
 
 function BookingCard ({booking}) {
+
     const dispatch = useDispatch()
     const spot = useSelector(state=> state.spots.singleSpot)
     let previewImage;
     if(spot.spotImages && spot.spotImages.length> 0){
         previewImage = spot.spotImages[0]
     }
+
     useEffect(()=>{
         dispatch(getOneSpot(booking.spotId))
     }, [])
 
-    if(!spot) return null
+    if(!spot.id) return null
     return(
-        <div>
-            <div>
+        <div className="bookingCard">
+
+            <div className="bookingCardDetails">
+                <div className="bookingCardHeader">
+                    <div className="bookingCardName">
+                        <span>{spot.name}</span>
+                    </div>
+                    <div className="bookingCardDetail smallText">
+                        <span>Hosted by {spot.Owner.firstName}</span>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                            <span>{dateFormatter.format(Date.parse(booking.startDate))}</span>
+                        </div>
+                        <div>
+                            <span>-</span>
+                        </div>
+                        <div>
+                            <span>{dateFormatter.format(Date.parse(booking.endDate))}</span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="bookingCardDetail">
+                            <span>{spot.city}, {spot.state}</span>
+                        </div>
+                        <div className="bookingCardDetail">
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bookingCardImage">
                 {spot.spotImages?.length!==0 &&
                     <img src={previewImage.url}></img>
                 }
